0ee4987cb9766b4d7397949c3920a095c4648143
Smit Gabnai <gabanismit11@gmail.com>
2022-07-06 22:39:35 -0400

    keybord not functional

---
+++ src/Components/Layout/Keyboard.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+export default function Keyboard() {
+  return (
+    <div class="base">
+		<div class="line1">
+			<span>Q</span>
+			<span>W</span>
+			<span>E</span>
+			<span>R</span>
+			<span>T</span>
+			<span>Y</span>
+			<span>U</span>
+			<span>I</span>
+			<span>O</span>
+			<span>P</span>
+		</div>
+		<div class="line2">
+			<span>A</span>
+			<span>S</span>
+			<span>D</span>
+			<span>F</span>
+			<span>G</span>
+			<span>H</span>
+			<span>J</span>
+			<span>K</span>
+			<span>L</span>
+		</div>
+		<div class="line3">
+
+			<span>Z</span>
+			<span>X</span>
+			<span>C</span>
+			<span>V</span>
+			<span>B</span>
+			<span>N</span>
+			<span>M</span>
+
+		</div>
+		<div class="line4">
+			<span>123</span>
+
+			<span>Space</span>
+			<span>Return</span>
+		</div>
+	</div>
+  )
+}
